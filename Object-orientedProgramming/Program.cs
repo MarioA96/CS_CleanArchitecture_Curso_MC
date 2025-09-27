ed88cdb3d3ce286675e30ed84b3fffad8fa36189
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-09-26 21:33:33 -0600

    ObjectOrientedProgramming
    Paradigma de programacion Orientada a Objetos.

---
+++ Object-orientedProgramming/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Beer erdingerBeer = new Beer()
+{
+    Name = "Erdinger",
+    Price = 3,
+};
+
+var coronaBeer = new Beer();
+coronaBeer.Name = "Corona";
+coronaBeer.Price = 1.5m;
+
+System.Console.WriteLine(erdingerBeer.Name);
+System.Console.WriteLine(coronaBeer.Name + " $ " + coronaBeer.Price);
+System.Console.WriteLine(erdingerBeer.GetInfo());
+
+public class Beer
+{
+    public string? Name { get; set; }
+    public decimal? Price { get; set; }
+
+    public string GetInfo()
+    {
+        return "Nombre: " + Name + ", Precio: $" + Price;
+    }
+}
