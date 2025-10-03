b81f0982a6d89bab07b4748e57dabb2b0a63f58f
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-02 18:24:36 -0600

    Refactor Beer class and initialization; add alcohol handling and update GetInfo method

--- Object-orientedProgramming/Program.cs
+++ Object-orientedProgramming/Program.cs
@@ -1,25 +1,51 @@
 ﻿
-Beer erdingerBeer = new Beer()
-{
-    Name = "Erdinger",
-    Price = 3,
-};
+Beer erdingerBeer = new Beer("Erdinger", 3, -2);
+// {
+//     Name = ,
+//     Price = 3,
+// };
 
-var coronaBeer = new Beer();
-coronaBeer.Name = "Corona";
-coronaBeer.Price = 1.5m;
+// var coronaBeer = new Beer("Corona", 1.5m);
+// coronaBeer.Name = "Corona";
+// coronaBeer.Price = 1.5m;
 
 System.Console.WriteLine(erdingerBeer.Name);
-System.Console.WriteLine(coronaBeer.Name + " $ " + coronaBeer.Price);
+// System.Console.WriteLine(coronaBeer.Name + " $ " + coronaBeer.Price);
 System.Console.WriteLine(erdingerBeer.GetInfo());
+System.Console.WriteLine(erdingerBeer.SAlcohol);
 
 public class Beer
 {
     public string? Name { get; set; }
     public decimal? Price { get; set; }
+    private decimal _alcohol;
+    public decimal Alcohol
+    {
+        get { return _alcohol; }
+        set
+        {
+            if (value < 0)
+            {
+                value = 0;
+            }
+            _alcohol = value;
+        }
+    }
+
+    public Beer(string name, decimal price, decimal alcohol)
+    {
+        Name = name;
+        Price = price;
+        Alcohol = alcohol;
+    }
+
+    public string SAlcohol
+    {
+        get { return "Alcohol: " + _alcohol.ToString(); }
+    }
 
     public string GetInfo()
     {
-        return "Nombre: " + Name + ", Precio: $" + Price;
+        return "Nombre: " + Name + ", Precio: $" + Price + ", Grados de alcohol: " + Alcohol;
     }
 }
