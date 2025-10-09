62765a4c42619cf36c3741167ab9248ac5b4dc23
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-08 20:03:48 -0600

    Remove commented-out Beer class code and clean up Program.cs

--- Object-orientedProgramming/Program.cs
+++ Object-orientedProgramming/Program.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using Object_orientedProgramming.Business;
+
+
 Beer erdingerBeer = new Beer("Erdinger", 3, -2);
 // {
 //     Name = ,
@@ -14,38 +16,3 @@ System.Console.WriteLine(erdingerBeer.Name);
 System.Console.WriteLine(erdingerBeer.GetInfo());
 System.Console.WriteLine(erdingerBeer.SAlcohol);
 
-public class Beer
-{
-    public string? Name { get; set; }
-    public decimal? Price { get; set; }
-    private decimal _alcohol;
-    public decimal Alcohol
-    {
-        get { return _alcohol; }
-        set
-        {
-            if (value < 0)
-            {
-                value = 0;
-            }
-            _alcohol = value;
-        }
-    }
-
-    public Beer(string name, decimal price, decimal alcohol)
-    {
-        Name = name;
-        Price = price;
-        Alcohol = alcohol;
-    }
-
-    public string SAlcohol
-    {
-        get { return "Alcohol: " + _alcohol.ToString(); }
-    }
-
-    public string GetInfo()
-    {
-        return "Nombre: " + Name + ", Precio: $" + Price + ", Grados de alcohol: " + Alcohol;
-    }
-}
