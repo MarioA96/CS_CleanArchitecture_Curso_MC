b7d3f5496523aec1540de8702fd2302fa7287956
QUIROZ SOLDAN MARIO ALBERTO <marioaquirozs@gmail.com>
2025-10-29 22:13:56 -0600

    Add Operations class: implement static methods for addition and multiplication

---
+++ Object-orientedProgramming/Business/Operations.cs
@@ -0,0 +1,9 @@
+
+namespace Object_orientedProgramming.Business
+{
+    public class Operations
+    {
+        public static decimal Add(decimal a, decimal b) => a + b;
+        public static decimal Mul(decimal a, decimal b) => a * b;
+    }
+}
